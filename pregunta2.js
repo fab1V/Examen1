3f59b74ab142a12d66e20956e66e9f513862a117
Fab1V0 <20224297@aloe.ulima.edu.pe>
2025-04-28 21:33:31 -0500

    Medio finales

---
+++ pregunta2.js
@@ -0,0 +1,10 @@
+function link() {
+    const URL = document.getElementById('URL').value.trim();
+    const imagen = document.getElementById('image').value.trim();
+    
+    if (URL=<img src=''/>
+    ) {
+      imagen.textContent = <img src=''/>;
+    } 
+
+    }
